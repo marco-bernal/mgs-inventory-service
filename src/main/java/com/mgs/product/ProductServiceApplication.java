73dad7aa2e69daeb581db6fd0b8991963d9d2532
marco-bernal <bernal.marco.a.84@gmail.com>
2024-10-28 17:50:09 -0600

    PS3 - Add db config for local deployment

--- src/main/java/com/mgs/product/ProductServiceApplication.java
+++ src/main/java/com/mgs/product/ProductServiceApplication.java
@@ -1,13 +1,13 @@
-package com.mgs.inventory;
+package com.mgs.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InventoryServiceApplication {
+public class ProductServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InventoryServiceApplication.class, args);
+		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
 }
